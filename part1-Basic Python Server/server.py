ef28a76f9caec1d5f129190450ae09c8b0276b70
QaysTrad <qaystrad@gamil.com>
2018-07-18 14:22:08 +0300

    finished part 1

--- part1-Basic Python Server/server.py
+++ part1-Basic Python Server/server.py
@@ -1,19 +1,19 @@
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from http.server import BaseHTTPRequestHandler,HTTPServer
 #Start by importing myHandler Class from requestHandler.py file
-
+from requestHandler import myHandler
 #Every server needs to listen on a port with a unique number. The
 #standard port for HTTP servers is port 8000,
-PORT_NUMBER = 8080
+PORT_NUMBER = 8000
 
 try:
 	#This statement creates a web server and define the handler to manage the
 	#incoming request
-	server = HTTPServer(('', PORT_NUMBER), 'Fill_Me_In')
-	print 'Started httpserver on port ' , PORT_NUMBER
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print('Started httpserver on port ' , PORT_NUMBER)
 	
 	#Wait forever for incoming htto requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
-	print '^C received, shutting down the web server'
+	print('^C received, shutting down the web server')
 	server.socket.close()
